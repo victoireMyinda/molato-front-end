3de5be8cccae6e7f81787ac484d6709c278d2997
gloriane98 <glorianek66@gmail.com>
2022-11-14 08:19:11 +0100

    feat:create login page, page error and dashoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+// import React, { useContext } from 'react';
 import HomePage from './pages/HomePage';
-import Login from './pages/LoginPage'
-import './App.css';
+import Login from './pages/LoginPage';
 import Auth from './pages/AuthPage';
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom';
+// import { UserLoginStatusContext } from './Context/userLoginStatus';
+import Error from './pages/PageError';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
+/*   const {isLogin} = useContext(UserLoginStatusContext);
+  const isLogedStorage = localStorage.getItem('isLogin');
+
+  if ((isLogin===false) && (isLogedStorage !== 'true')) {
+    return (
+      <>
+        <Login/>
+        <Auth/>
+      </>
+    )
+  } */
+
+
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/auth' element={<Auth/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='*' element={<Error/>}/>
       </Routes>
     </div>
   );
